2f55ee2c65a91b9d5a3f19550e6c29629f379d98
6810779s <81551986+6810779s@users.noreply.github.com>
2021-06-12 21:02:28 +0900

    edit

--- JS/style.js
+++ JS/style.js
@@ -1,40 +1,42 @@
 let slide_container = document.querySelector('#profile .slider_container');
 let count = 0;
 
-setInterval(function(){
+setInterval(function () {
   slide_container.style.left = -count * 1200 + "px";
-  if(count===3){
+  if (count === 3) {
     slide_container.style.left = 0 + "px";
-    count=-1;
+    count = -1;
   }
   count++;
-},4000);
+}, 4000);
 
 let slide_container2 = document.querySelector('#profile2 .slider_container');
-let count2=0;
-setInterval(function(){
+let count2 = 0;
+setInterval(function () {
   slide_container2.style.left = -count2 * 800 + "px";
-  if(count2===3){
+  if (count2 === 3) {
     slide_container2.style.left = 0 + "px";
-    count2=-1;
+    count2 = -1;
   }
   count2++;
-},4000);
+}, 4000);
 
-document.addEventListener('scroll', function(){
-  let java_box = document.querySelector('.skill_container2 .java_box');
-  let html_box = document.querySelector('.skill_container2 .html_box');
-  let css_box = document.querySelector('.skill_container2 .css_box');
-  let c_box = document.querySelector('.skill_container2 .c_box');
-  let python_box = document.querySelector('.skill_container2 .python_box');
-  let JS_box = document.querySelector('.skill_container2 .JS_box');
-  let mySQL_box = document.querySelector('.skill_container2 .mySQL_box');
-  let Android_box = document.querySelector('.skill_container2 .Android_box');
+document.addEventListener('scroll', function () {
+  const java_box = document.querySelector('.skill_container2 .java_box');
+  const html_box = document.querySelector('.skill_container2 .html_box');
+  const css_box = document.querySelector('.skill_container2 .css_box');
+  const c_box = document.querySelector('.skill_container2 .c_box');
+  const python_box = document.querySelector('.skill_container2 .python_box');
+  const JS_box = document.querySelector('.skill_container2 .JS_box');
+  const mySQL_box = document.querySelector('.skill_container2 .mySQL_box');
+  const Android_box = document.querySelector('.skill_container2 .Android_box');
 
   let currentScrollValue = document.documentElement.scrollTop;
 
-  if (currentScrollValue>=1000){
- 
+
+  if (currentScrollValue >= 1000) {
+    
+    // zooms();
     java_box.style.animationPlayState = "running";
     html_box.style.animationPlayState = "running";
     css_box.style.animationPlayState = "running";
@@ -44,7 +46,7 @@ document.addEventListener('scroll', function(){
     mySQL_box.style.animationPlayState = "running";
     html_box.style.animationPlayState = "running";
     Android_box.style.animationPlayState = "running";
-  }else{
+  } else {
 
     java_box.style.animationPlayState = "paused";
     html_box.style.animationPlayState = "paused";
@@ -59,9 +61,28 @@ document.addEventListener('scroll', function(){
 });
 
 
+let hamburger = document.querySelector('#hidden_menu .hamburger');
+let hiddenMenu = document.querySelector('#hidden_menu .hiddenMenu');
+let btn = 0;
+hamburger.addEventListener('click',function(){
+  if(btn===0){
+    hiddenMenu.style.display = "block";
+    btn=1;
+  }else{
+    hiddenMenu.style.display = "none";
+    btn=0;
+  }
+  
+});
+
+document.addEventListener('scroll',function(){
+  hiddenMenu.style.display = "none";
+  btn=0;
+});
+
 
 AOS.init({
-  easing:'ease-out-back',
+  easing: 'ease-out-back',
   duration: 3000
 });;
 
